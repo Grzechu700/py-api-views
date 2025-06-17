8a920057d54b222a384286f6915dca55a32cea64
Marcin <grzechnikmarcin@wp.pl>
2025-06-17 11:13:29 +0200

    Solution

--- cinema/tests/test_actor_api.py
+++ cinema/tests/test_actor_api.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIClient
 
 from cinema.serializers import ActorSerializer
 from cinema.models import Actor
-from cinema.views import ActorList, ActorDetail
+from cinema.views import ActorListCreate, ActorDetail
 
 
 class ActorApiTests(TestCase):
